08516014c1d214352b57140e81cd7bad5c1d9894
shrewd888 <61401856+shrewd888@users.noreply.github.com>
2020-02-23 22:04:32 -0800

    Update Portfolio.java

--- src/main/java/Portfolio.java
+++ src/main/java/Portfolio.java
@@ -1,22 +1,15 @@
-import org.apache.commons.math3.distribution.NormalDistribution;
-import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-
 import java.math.BigDecimal;
 
 public class Portfolio {
 
-    //name, mean and standardDeviation of portfolio
     private PortfolioType portfolioType;
 
     private BigDecimal initialInvestment;
 
-    // median over x number year
     private double median;
 
-    // 90 percentile value among the x-times simulations
     private double tenPercentBestCase;
 
-    //10 percentile value among the x-times simulations
     private double tenPercentWorstCase;
 
     public Portfolio() {}
@@ -65,35 +58,6 @@ public class Portfolio {
 
     public void setTenPercentWorstCase(double tenPercentWorstCase) {
         this.tenPercentWorstCase = tenPercentWorstCase;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Portfolio)) return false;
-
-        Portfolio portfolio = (Portfolio) o;
-
-        if (Double.compare(portfolio.getMedian(), getMedian()) != 0) return false;
-        if (Double.compare(portfolio.getTenPercentBestCase(), getTenPercentBestCase()) != 0) return false;
-        if (Double.compare(portfolio.getTenPercentWorstCase(), getTenPercentWorstCase()) != 0) return false;
-        if (getPortfolioType() != portfolio.getPortfolioType()) return false;
-        return getInitialInvestment() != null ? getInitialInvestment().equals(portfolio.getInitialInvestment()) : portfolio.getInitialInvestment() == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = getPortfolioType() != null ? getPortfolioType().hashCode() : 0;
-        result = 31 * result + (getInitialInvestment() != null ? getInitialInvestment().hashCode() : 0);
-        temp = Double.doubleToLongBits(getMedian());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(getTenPercentBestCase());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(getTenPercentWorstCase());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
     }
 
     @Override
